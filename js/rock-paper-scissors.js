b9535ff3c3e544b03e45b351fdb39714d53e880d
Joel C <104279162+JoelCarriere@users.noreply.github.com>
2024-01-17 15:53:17 -0500

    Create final functions required for rps game
    Fucntions were created to play a round and to
    play a game of 5 rounds

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,15 +3,80 @@
 //Create a function that will randomly return rock, paper, or scissors
 //Choose randomly with a number, based on the number value choose a string to go along with it
 function getComputerChoice() {
-    let computerChoice = Math.random()
-    let choice
-    if (computerChoice <= 0.333) {
-        return choice = "Rock";
+    let choice = Math.random()
+    let computerSelection
+    if (choice <= 0.333) {
+        computerSelection = 'rock';
     }
-    else if (computerChoice <= 0.666) {
-        return choice = "Paper";
+    else if (choice <= 0.666) {
+        computerSelection = 'paper';
     }
     else {
-        return choice = "Scissors";
+        computerSelection = 'scissors';
+    }
+    return computerSelection;
+}
+
+//Create a function that plays a round of the game and outputs the win/loss outcome
+//This will take in the user input and computer input
+function playRound (playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    if (computerSelection === player){
+        return ('Tie game! You both chose ' + playerSelection.toUpperCase(0));
+    }
+    else if (player == 'paper'){
+        if (computerSelection == 'scissors'){
+            return 'You Lose! Scissors beats Paper';
+            computerScore++
+        }
+        else {
+            return 'You Win! Paper beats Rock';
+            playerScore++
+        }
+    }
+    else if (player == 'rock'){
+        if (computerSelection == 'paper'){
+            return 'You Lose! Paper beats Rock';
+            computerScore++
+        }
+        else {
+            return 'You Win! Rock beats Scissors';
+            playerScore++
+        }
+    }
+    else if (player == 'scissors'){
+        if (computerSelection == 'rock'){
+            return 'You Lose! Rock beats Scissors';
+            computerScore++
+        }
+        else {
+            return 'You Win! Scissors beats Paper';
+            playerScore++
+        }
+    }
+
+}
+
+let computerScore = 0;
+let playerScore = 0;
+//Create a game function to determine a best of 5 winner for the game after 5 rounds
+function game () {
+
+    for (let step = 1; step <= 5; step++){
+        let playerSelection = prompt('Please choose rock, paper, or scissors and enter in the field below:');
+        computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+    }
+
+    if (playerScore > computerScore) {
+        return "Player wins!"
+    }
+    else {
+        return "Computer wins!"
     }
 }
+
+//Create a function that accepts users choice of rock, paper, or scissors
+//function playerSelection() {
+
